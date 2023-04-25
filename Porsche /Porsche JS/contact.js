2324672d67ca604260b1b67afa458632169d5871
kaziot89 <119735227+kaziot89@users.noreply.github.com>
2023-04-26 01:30:00 +0200

    Add files via upload

---
+++ Porsche /Porsche JS/contact.js
@@ -0,0 +1,13 @@
+const contactForm = document.getElementById("contact-form");
+
+contactForm.addEventListener("submit", function (event) {
+  event.preventDefault();
+  const name = document.getElementById("name").value;
+  const email = document.getElementById("email").value;
+  const message = document.getElementById("message").value;
+
+  if (name === "" || email === "" || message === "") {
+    alert("Wypełnij wszystkie pola.");
+  } else {
+  }
+});
---
+++ Porsche /Porsche JS/index.js
@@ -0,0 +1,3 @@
+function redirectToPage(buttonNumber) {
+  window.location.href = `carCard.html?id=${buttonNumber}`;
+}
